544f787d0e0e1110389ead40c76cb83f8634de41
George Silvis, III <george.iii.silvis@gmail.com>
2012-05-13 01:18:31 -0400

    Fixed multiple problems in level.c
    - Zero a field before sscanf'ing into it with %ms, which requires that the
      pointer given be one that can be passed to free
    
    - Move the fail condition into its own function, to avoid scoping and
      volatility problems.  (Also because it is nicer, I guess.)
    
    - Fix fail condition to free one string for each tile, rather than the last
      tile repeatedly.

--- level.c
+++ level.c
@@ -34,6 +34,15 @@ int level_get_line (FILE *f, char **line_ptr, size_t *size)
     return level_get_line(f, line_ptr, size);
 }
 
+SavedTile *level_fail (SavedTile *r, size_t r_used, char *l)
+{
+  for (size_t i = 0; i < r_used; i++) {
+    free(r[i].path);
+  }
+  free(r);
+  free(l);
+  return NULL;
+}
 
 SavedTile *level_parse_file (FILE *f, int *num_tiles, int *u)
 {
@@ -43,9 +52,8 @@ SavedTile *level_parse_file (FILE *f, int *num_tiles, int *u)
   size_t r_size = 4;
   size_t r_used = 0;
 
-  /* Yes, I used two goto's for the two fail conditions.  Eat it. */
-
-  if (num_tiles == NULL || u == NULL)  goto fail;
+  if (num_tiles == NULL || u == NULL)
+    return level_fail(returnee, r_used, line_ptr);
 
   returnee = malloc(r_size * sizeof(SavedTile));
 
@@ -59,16 +67,22 @@ SavedTile *level_parse_file (FILE *f, int *num_tiles, int *u)
       if (line_ptr[0] == LF_KEYVALUE_SIGNAL)
         {
           if (line_ptr[1] == LF_KEYVALUE_SIGNAL)
-            goto fail; /* could not parse mandatory field. */
+            return level_fail(returnee, r_used, line_ptr);
+          /* could not parse mandatory field. */
           else
-            continue; /* could not parse optional field. */
+            continue;
+          /* could not parse optional field. */
         }
+
+      returnee[r_used].path = NULL; /* required to use the %ms sscanf
+                                       extension */
 
       if (sscanf(line_ptr,
                  "%hhd"LF_DELIM"%hhd"LF_DELIM"%m[a-zA-Z]",
                  &(returnee[r_used].tile_type),
                  &(returnee[r_used].agent),
-                 &(returnee[r_used].path)) < 2)  goto fail;
+                 &(returnee[r_used].path)) < 2)
+        return level_fail(returnee, r_used, line_ptr);
 
 
       if ((returnee[r_used].tile_type == TILE_TYPE_TARGET) &&
@@ -89,22 +103,14 @@ SavedTile *level_parse_file (FILE *f, int *num_tiles, int *u)
         }
     }
 
-  if (audit)
-    goto fail; /* Different number of blocks and targets: bad level. */
-
+  if (audit) /* Different number of blocks and targets: bad level. */
+    return level_fail(returnee, r_used, line_ptr);
+  
+  /* trim returnee to be the right size */
   returnee = realloc(returnee, r_used * sizeof(SavedTile));
 
   *num_tiles = r_used;
   *u = unsolved;
   free(line_ptr);
   return returnee; /* Success */
-
-
- fail:
-  for (size_t i = 0; i < r_used; i++) {
-    free(returnee[r_used].path);
-  }
-  free(returnee);
-  free(line_ptr);
-  return NULL;
 }
